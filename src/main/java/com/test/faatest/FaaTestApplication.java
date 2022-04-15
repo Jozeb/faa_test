c42cd9f0705d5865d9d0362cc7e28b1e2c7aa8ab
Jazib Hassan <jazibulhassan95@gmail.com>
2022-04-15 06:31:16 +0400

    First commit

---
+++ src/main/java/com/test/faatest/FaaTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.faatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaaTestApplication.class, args);
+	}
+
+}
